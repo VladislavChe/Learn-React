906bf350c4a7c261e10e599be69f8be57afc4e01
Vladislav <cheremisin0610@mail.ru>
2021-10-26 00:33:01 +0300

    lesson 14: CSS Modules

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import s from './Navbar.module.css';
 
 const Navbar = () => {
   return (
-    <nav className="nav">
-      <a>Profile</a>
-      <a>Messenges</a>
-      <a>News</a>
-      <a>Music</a>
-      <a>Settings</a>
+    <nav className={s.nav}>
+      <div className={s.item}>
+        <a>Profile</a>
+      </div>
+      <div className={s.item}>
+        <a>Messages</a>
+      </div>
+      <div className={s.item}>
+        <a>News</a>
+      </div>
+      <div className={s.item}>
+        <a>Music</a>
+      </div>
+      <div className={s.item}>
+        <a>Settings</a>
+      </div>
     </nav>
   );
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={s.content}>
       <div>
         <img
           src="https://cf.bstatic.com/data/xphoto/1182x887/202/20233869.jpg?size=S"
           alt="foto"
         />
       </div>
-      <div>
-        ava + description
-        <img src="" alt="" />
-      </div>
+      <div>ava + description</div>
       <div>My posts</div>
       <div>New post</div>
-      <div>
-        <div>post1</div>
-        <div>post2</div>
+      <div className={s.posts}>
+        <div className={s.item}>post1</div>
+        <div className={s.item}>post2</div>
       </div>
     </div>
   );
