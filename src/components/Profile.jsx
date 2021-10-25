d0b2d4b17d0d9e958903f3d4659b5ed1b4c6f9a6
Vladislav <cheremisin0610@mail.ru>
2021-10-25 21:53:08 +0300

    lesson 12 - create components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://cf.bstatic.com/data/xphoto/1182x887/202/20233869.jpg?size=S"
+          alt="foto"
+        />
+      </div>
+      <div>
+        ava + description
+        <img src="" alt="" />
+      </div>
+      <div>My posts</div>
+      <div>New post</div>
+      <div>
+        <div>post1</div>
+        <div>post2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
