eb2a8ab006dab5ab0f4be448b3e3bda39403c0b0
Vladislav <cheremisin0610@mail.ru>
2021-11-07 19:52:44 +0300

    props posts

--- src/components/Profiles/Profile.jsx
+++ src/components/Profiles/Profile.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import MyPosts from './myPosts/MyPosts';
+import MyPost from './myPosts/Post/MyPost';
 import s from './Profile.module.css';
 import Profileinfo from './Profileinfo/Profileinfo';
 
 const Profile = () => {
+  let posts = [
+    { id: 1, message: 'HI, how are you?', likeCount: 12 },
+    { id: 2, message: "It's my first post", likeCount: 11 },
+    { id: 2, message: 'Yo', likeCount: 13 },
+    { id: 2, message: 'Blala', likeCount: 15 },
+  ];
+
+  let postsElements = posts.map((post) => (
+    <MyPost message={post.message} likeCount={post.likeCount} />
+  ));
   return (
     <div className={s.profile__content}>
       <Profileinfo />
 
-      <MyPosts />
+      <MyPosts posts={postsElements} />
     </div>
   );
 };
--- src/components/Profiles/myPosts/MyPosts.jsx
+++ src/components/Profiles/myPosts/MyPosts.jsx
@@ -1,19 +1,7 @@
 import React from 'react';
 import s from './MyPosts.module.css';
-import MyPost from './Post/MyPost';
 
-const MyPosts = () => {
-  let posts = [
-    { id: 1, message: 'HI, how are you?', likeCount: 12 },
-    { id: 2, message: "It's my first post", likeCount: 11 },
-    { id: 2, message: 'Yo', likeCount: 13 },
-    { id: 2, message: 'Blala', likeCount: 15 },
-  ];
-
-  let postsElements = posts.map((post) => (
-    <MyPost message={post.message} likeCount={post.likeCount} />
-  ));
-
+const MyPosts = (props) => {
   return (
     <div className={s.myposts}>
       <h3>My posts</h3>
@@ -25,7 +13,7 @@ const MyPosts = () => {
           <button>Add post</button>
         </div>
       </div>
-      <div className={s.myposts__posts}>{postsElements}</div>
+      <div className={s.myposts__posts}>{props.posts}</div>
     </div>
   );
 };
