25216227d21f8128681ba14a460ad34f0bbec193
Vladislav <cheremisin0610@mail.ru>
2022-02-20 19:29:13 +0300

    add transfer to user profile

--- src/components/Profiles/Profileinfo/Profileinfo.jsx
+++ src/components/Profiles/Profileinfo/Profileinfo.jsx
@@ -16,8 +16,25 @@ const Profileinfo = (props) => {
         />
       </div>
       <div className={s.profileinfo__description}>
-        <img alt="avatar" src={props.profile.photos.large} />
-        ava + description
+        <img alt="avatar" src={props.profile.photos.small} />
+        <div className={s.profileinfo__descriptionWrapp}>
+          <div>
+            <span>Name:</span> {props.profile.fullName}
+          </div>
+          <div>
+            <span>status:</span> {props.profile.aboutMe}
+          </div>
+          <div>
+            <span>working:</span> {props.profile.lookingForAJob === true ? 'не работаю' : 'работаю'}
+          </div>
+          <div>
+            <span>VK:</span> <a href="https://www.vk.com/">{props.profile.contacts.vk}</a>
+          </div>
+          <div>
+            <span>instagram:</span>{' '}
+            <a href="https://www.instagram.com/">{props.profile.contacts.instagram}</a>
+          </div>
+        </div>
       </div>
     </div>
   );
