f3b01b5743c5c609c6dab8ccee10411348d64c57
Vladislav <cheremisin0610@mail.ru>
2022-02-20 21:53:45 +0300

    add Authorization

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import ProfileContainer from './components/Profiles/ProfileContainer';
 import News from './components/News/News';
@@ -9,12 +8,13 @@ import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 const App = (props) => {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
-        <Header />
+        <HeaderContainer />
         <Navbar />
         <div className="app-wrapper-content">
           <Route exact path="/" render={() => <ProfileContainer />} />
